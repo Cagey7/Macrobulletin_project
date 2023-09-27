536a14f3e8c9e2c2041396bee2c4268ee229e1ca
Cagey7 <dagodecagey@gmail.com>
2023-09-28 01:22:05 +0600

    Added new table

--- fill_tables.py
+++ fill_tables.py
@@ -88,9 +88,7 @@ class FillTable(Automation):
                 AND countries = 'Всего'
                 AND activity_type = 'Промышленность'
                 AND industrial_products_list = 'Всего'
-                AND created_at BETWEEN '{start_year}-{start_month}-{start_last_day}' AND '{end_year}-{end_month}-{end_last_day}'
-            ORDER BY 
-                created_at;
+                AND created_at BETWEEN '{start_year}-{start_month}-{start_last_day}' AND '{end_year}-{end_month}-{end_last_day}';
             """
 
             self.cur.execute(join_views, [item[1] for item in bull_name])
@@ -108,9 +106,9 @@ class FillTable(Automation):
             months_list = [""]
 
             data_for_excel = []
-            for bull_name_i, economic_index in enumerate(bull_name):
+            for bull_name_i, name in enumerate(bull_name):
                 index_data = []
-                index_data.append(economic_index[0])
+                index_data.append(name[0])
                 for year in range(start_year, end_year+1):
                     for i, m in enumerate(months, start=1):
                         if year == start_year and i < start_month: continue
@@ -123,7 +121,7 @@ class FillTable(Automation):
                         for data in sorted_data:
                             t_month, t_year, _ = data[2].split()
                             t_year = int(t_year)
-                            if economic_index[1] == data[0] and t_month == m[1] and t_year == year:
+                            if name[1] == data[0] and t_month == m[1] and t_year == year:
                                 index_data.append(float(data[1]))
                                 data_not_exist = False
                         if data_not_exist:
@@ -154,7 +152,145 @@ class FillTable(Automation):
             workbook.close()
 
 
+    def avsalary_byactivity_fill_table(self, start_year=None, end_year=None):
+        excel_path = os.path.join(self.excel_path, "average_salary_by_economic_activity_in_thousand_tenge.xlsx")
+        workbook = Workbook()
+        sheet = workbook.active
+
+        if end_year is None:
+            current_date = datetime.now()
+            end_year = current_date.year
+        
+        if start_year is None:
+            current_date = datetime.now()
+            start_year = current_date.year - 3
+
+        bull_name = [
+            ["Республика Казахстан","Всего"],
+            ["Сельское, лесное и рыбное хозяйство","Сельское, лесное и рыбное хозяйство"],
+            # ["Промышленность", "Промышленность"],
+            ["Горнодобывающая промышленность","Горнодобывающая промышленность и разработка карьеров"],
+            ["Обрабатывающая промышленность","Обрабатывающая промышленность",],
+            ["Снабжение электроэнергией","Снабжение электроэнергией, газом, паром, горячей водой и кондиционированным воздухом"],
+            ["Водоснабжение","Водоснабжение; сбор, обработка и удаление отходов, деятельность по ликвидации загрязнений"],
+            ["Строительство","Строительство"],
+            ["Оптовая и розничная торговля","Оптовая и розничная торговля; ремонт автомобилей и мотоциклов"],
+            ["Транспорт и складирование","Транспорт и складирование"],
+            ["Предоставление услуг по проживанию и питанию","Предоставление услуг по проживанию и питанию"],
+            ["Информация и связь","Информация и связь"],
+            ["Финансовая и страховая деятельность","Финансовая и страховая деятельность"],
+            ["Операции с недвижимым имуществом","Операции с недвижимым имуществом"],
+            ["Профессиональная, научная и техническая деятельность","Профессиональная, научная и техническая деятельность"],
+            ["Деятельность в области административного обслуживания","Деятельность в области административного и вспомогательного обслуживания"],
+            ["Государственное управление и оборона","Государственное управление и оборона; обязательное  социальное обеспечение"],
+            ["Образование","Образование"],
+            ["Здравоохранение","Здравоохранение и социальное обслуживание населения"],
+            ["Искусство, развлечения и отдых","Искусство, развлечения и отдых"],
+            ["Предоставление прочих видов услуг","Предоставление прочих видов услуг"]
+        ]
+
+
+        # Создание мест списку для поиска
+        condition = ",".join(["%s"] * len(bull_name))
+    
+        join_views = f'''
+        SELECT 
+            activity_type,
+            value,
+            description,
+            created_at
+        FROM 
+            avmon_nom_wages_year 
+        WHERE 
+            region = 'РЕСПУБЛИКА КАЗАХСТАН' 
+            AND activity_type IN ({condition}) 
+            AND terrain_type = 'Всего' 
+            AND enterprise_dimension = 'Всего' 
+            AND gender = 'Всего' 
+            AND created_at BETWEEN '{start_year}-01-01' AND '{start_year}-12-31'
+        UNION
+        SELECT 
+            activity_type, 
+            value, 
+            description, 
+            created_at 
+        FROM
+            avmon_nom_wages_quarter 
+        WHERE 
+            region = 'РЕСПУБЛИКА КАЗАХСТАН' AND activity_type IN ({condition})
+            AND economic_sectors = 'Экономика в целом'
+            AND created_at BETWEEN '{start_year+1}-01-01' AND '{end_year+1}-12-01';
+        '''
+        condition_values = []
+        for _ in range(2):
+            for item in bull_name:
+                condition_values.append(item[1])
+
+        self.cur.execute(join_views, condition_values)
+        
+        sorted_data = self.cur.fetchall()
+
+        latest_date = max(sorted_data, key=lambda x: x[3])
+
+        for data in sorted_data:
+            if data == latest_date:
+                last_quarter = int(data[2][0])
+                break
+
+        quarter_list = ["в тенге", "Год"]
+
+        data_for_excel = []
+        for bull_name_i, name in enumerate(bull_name):
+            index_data = []
+            index_data.append(name[0])
+            data_not_exist = True
+            for data in sorted_data:
+                if len(data[2].split()) == 2:
+                    if data[0] == name[1]:
+                        index_data.append(float(round(data[1]/1000, 1)))
+                        data_not_exist = False
+            if data_not_exist:
+                index_data.append("")
+            
+            for year in range(start_year+1, end_year+1):
+                end_quarter = 5
+                if year == end_year: end_quarter = last_quarter + 1
+                for quarter in range(1, end_quarter):
+                    data_not_exist = True
+                    for data in sorted_data:
+                        if data[2] == f"{quarter} квартал {year} г." and data[0] == name[1]:
+                            index_data.append(float(round(data[1]/1000, 1)))
+                            data_not_exist = False
+                            if bull_name_i == 0:
+                                quarter_list.append(f"{quarter} кв")
+                    if data_not_exist:
+                        index_data.append("")
+            data_for_excel.append(index_data)
+
+        # заголовок
+        year_list = list(range(start_year+1, end_year+1))
+        start_cell = 3
+        end_cell = 6
+        sheet.cell(row=1, column=1, value="")
+        sheet.cell(row=1, column=2, value=start_year)
+        for i, year in enumerate(year_list):
+            sheet.cell(row=1, column=start_cell, value=year)
+            sheet.merge_cells(start_row=1, start_column=start_cell, end_row=1, end_column=end_cell)
+            start_cell = end_cell+1
+            if i+2 == len(year_list):
+                end_cell += last_quarter
+            else:
+                end_cell += 4
+        
+        sheet.append(quarter_list)
+        for data in data_for_excel:
+            sheet.append(data)
+                
+        workbook.save(excel_path)
+        workbook.close()
+
 fillTable = FillTable("testtest", "postgres", "123456")
 
-fillTable.consumer_price_index_fill_table()
+#fillTable.avsalary_byactivity_fill_table()
+#fillTable.consumer_price_index_fill_table()
 fillTable.db_disconnect()
